3ec5915c019bc39cc50cdbfdd0a3a333ee32e9ee
Gonzalo Negro <gonzalonnegro@gmail.com>
2023-02-01 15:23:36 -0300

    Entregable node

--- Desafio clase 32/src/profilling/scripts/script1.sh
+++
@@ -1,4 +0,0 @@
-curl -X GET "http://localhost:8080/info"
-
-artillery quick --count 50 -n 20 "http://localhost:8080/info" > result_profilling.txt
-
--- Desafio clase 32/src/profilling/scripts/script2.sh
+++
@@ -1,6 +0,0 @@
-curl -X GET "http://localhost:8080/info"
-
-autocannon -c 100 -d 20 'http://localhost:8080/info'
-    # -c es el nro de clientes en simultaneo conectados
-    # -d es el tiempo que va a estar corriendo autocannon
-    
